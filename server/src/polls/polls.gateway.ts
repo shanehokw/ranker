c964960dad636342cbb74117020ac743f0391f7d
shanehokw <kenwae@hotmail.com>
2024-03-19 20:48:08 +0800

    add auth guard for polls

---
+++ server/src/polls/polls.gateway.ts
@@ -0,0 +1,42 @@
+import { Logger } from '@nestjs/common';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { PollsService } from './polls.service';
+import { Namespace, Socket } from 'socket.io';
+
+@WebSocketGateway({
+  namespace: 'polls',
+})
+export class PollsGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  private readonly logger = new Logger(PollsGateway.name);
+  constructor(private readonly pollsService: PollsService) {}
+
+  @WebSocketServer() io: Namespace;
+
+  afterInit(): void {
+    this.logger.log('Websocket Gateway initialized.');
+  }
+
+  handleConnection(client: Socket) {
+    const sockets = this.io.sockets;
+
+    this.logger.log(`WS Client with id: ${client.id} connected!`);
+    this.logger.debug(`Number of connected sockets: ${sockets.size}`);
+
+    this.io.emit('hello', `from ${client.id}`);
+  }
+
+  handleDisconnect(client: Socket) {
+    const sockets = this.io.sockets;
+
+    this.logger.log(`Disconnected socket id: ${client.id}`);
+    this.logger.debug(`Number of connected sockets: ${sockets.size}`);
+  }
+}
---
+++ server/src/socket-io-adapter.ts
@@ -0,0 +1,39 @@
+import { INestApplicationContext, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { IoAdapter } from '@nestjs/platform-socket.io';
+import { ServerOptions } from 'socket.io';
+
+export class SocketIOAdapter extends IoAdapter {
+  private readonly logger = new Logger(SocketIOAdapter.name);
+  constructor(
+    private app: INestApplicationContext,
+    private configService: ConfigService
+  ) {
+    super(app);
+  }
+
+  createIOServer(port: number, options?: ServerOptions) {
+    const clientPort = parseInt(this.configService.get('CLIENT_PORT'));
+
+    const cors = {
+      origin: [
+        `http://localhost:${clientPort}`,
+        new RegExp(`/^http:\/\/192\.168\.1\.([1-9]|[1-9]\d):${clientPort}$/`),
+      ],
+    };
+
+    this.logger.log(
+      'Configuring SocketIO server with custom CORS configuraton',
+      {
+        cors,
+      }
+    );
+
+    const optionsWithCors: ServerOptions = {
+      cors,
+      ...options,
+    };
+
+    return super.createIOServer(port, optionsWithCors);
+  }
+}
